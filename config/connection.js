16b94db72352d1ca78a9b41e5e430d89eb711e2d
DomenicoBarb <domenicobarberi585@gmail.com>
2023-03-17 19:44:52 -0400

    changed naming to match WIP index.js designation

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const database = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'nico585',
     database: 'employee_tracker_db'
 });
 
-module.exports = db;
+module.exports = database;
