0694b00ffd97426eda567bef97d81ff1a239ae84
DomenicoBarb <domenicobarberi585@gmail.com>
2023-03-17 23:13:37 -0400

    WIP schema/seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -13,12 +13,12 @@ INSERT INTO role (title, department, salary) VALUES
     ('Lawyer', 'Legal', 200000);
 
 -- Insert data into the employee table
-INSERT INTO employee (first_name, last_name, title, department, salary, manager) VALUES
-    ('Nico', 'Barberi', 'Software Engineer', 'Engineering', 90000, NULL),
-    ('Jony', 'Ive', 'Salesperson', 'Sales', 65000, NULL),
-    ('J.P', 'Morgan', 'Accountant', 'Finance', 110000, NULL),
-    ('Abraham', 'Lincoln', 'Lawyer', 'Legal', 200000, NULL),
-    ('Alice', 'Smith', 'Software Engineer', 'Engineering', 90000, 'Nico Barberi'),
-    ('Bob', 'Jones', 'Salesperson', 'Sales', 65000, 'Jony Ive'),
-    ('Charlie', 'Brown', 'Accountant', 'Finance', 110000, 'J.P Morgan'),
-    ('David', 'Lee', 'Lawyer', 'Legal', 200000, 'Abraham Lincoln');
+INSERT INTO employee (id, first_name, last_name, title, department, salary, manager) VALUES
+    (1, 'Nico', 'Barberi', 'Software Engineer', 'Engineering', 90000, NULL),
+    (2, 'Jony', 'Ive', 'Salesperson', 'Sales', 65000, NULL),
+    (3, 'J.P', 'Morgan', 'Accountant', 'Finance', 110000, NULL),
+    (4, 'Abraham', 'Lincoln', 'Lawyer', 'Legal', 200000, NULL),
+    (5, 'Alice', 'Smith', 'Software Engineer', 'Engineering', 90000, 'Nico Barberi'),
+    (6, 'Bob', 'Jones', 'Salesperson', 'Sales', 65000, 'Jony Ive'),
+    (7, 'Charlie', 'Brown', 'Accountant', 'Finance', 110000, 'J.P Morgan'),
+    (8, 'David', 'Lee', 'Lawyer', 'Legal', 200000, 'Abraham Lincoln');
